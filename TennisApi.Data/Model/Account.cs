5e25cc1a5b454da0f34fa3340379e198ee96c4af
Gustavo Ushjima <lgu85@hotmail.com>
2021-07-08 18:11:54 -0300

    api project

---
+++ TennisApi.Data/Model/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TennisApi.Data.Model
+{
+    public partial class Account
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
